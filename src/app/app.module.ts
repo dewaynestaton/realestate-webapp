949111311b854cb9640c78e1b05569141ed37fca
Dewayne Staton <dewayne@Dewaynes-MBP.lan>
2021-12-18 23:10:02 -0800

    Adding newly created project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuyComponent } from './admin/pages/buy/buy.component';
+import { RentComponent } from './admin/pages/rent/rent.component';
+import { SellComponent } from './admin/pages/sell/sell.component';
+import { SigninComponent } from './admin/pages/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponent,
+    RentComponent,
+    SellComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
