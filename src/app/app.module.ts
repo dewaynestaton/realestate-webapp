c23ffaaa6190574aaed14843b9169d4ac9faea11
Dewayne Staton <src.dewayne@gmail.com>
2021-12-21 20:39:53 -0800

    Wrote code for the login form and started with styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { BuyComponent } from './admin/pages/buy/buy.component';
 import { RentComponent } from './admin/pages/rent/rent.component';
 import { SellComponent } from './admin/pages/sell/sell.component';
 import { SigninComponent } from './admin/pages/signin/signin.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './admin/pages/register/register.component';
 import { NavbarComponent } from './admin/components/navbar/navbar.component';
 import { ForgotPasswordComponent } from './admin/pages/forgot-password/forgot-password.component';
 import { NotFoundComponent } from './admin/pages/not-found/not-found.component';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AngularMaterialModule } from './angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -29,9 +34,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
